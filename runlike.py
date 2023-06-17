c2e4e5c1afde0ff6902e3f9f68bac2e01ab7bb89
Stephen Griffith <sgriffith0891@gmail.com>
2023-06-17 02:38:50 -0400

    prettyfied code

--- runlike.py
+++ runlike.py
@@ -1,12 +1,12 @@
 import subprocess
 
-with open('containerIDs', 'r') as file:
-    IDs = file.read().split('\n')
+with open("containerIDs", "r") as file:
+    IDs = file.read().split("\n")
 
 IDs = [ID for ID in IDs if ID]
 
 for ID in IDs:
     print(ID)
 
-    with open(f'containerCommands/{ID}', 'w') as file:
-        subprocess.run(['runlike', ID], stdout = file)
+    with open(f"containerCommands/{ID}", "w") as file:
+        subprocess.run(["runlike", ID], stdout=file)
