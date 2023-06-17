a3bf2ab233e24f3a905a0e8e6dab4bc0ba55fd3a
Stephen Griffith <sgriffith0891@gmail.com>
2023-06-16 23:15:14 -0400

    expanded vhost script. Built Linode API integration

--- vhostPop/vhostPop.py
+++ vhostPop/vhostPop.py
@@ -4,14 +4,14 @@ import shutil
 from datetime import datetime as dt
 import os
 
-# NOTE: This script must be run adjacent to top directory where you want to locate compose files.
+from vhostPopConfig import *
+from linodeRecordCreate import *
 
-# Change these. Do not append paths with a /.
-defaultHost = 'isame-lab.com'
-defaultVhost = 'proxy_set_header Upgrade $http_upgrade;\nproxy_set_header Connection "upgrade";\nproxy_http_version 1.1;'
-logFile = 'vhost_pop_logs.log'
-containersPath = 'containers'
-vhostPath = 'containers/nginx/vhost.d'
+# Checks target directory recursively for compose files with a VIRTUAL_HOST environment variable and creates the corresponding vhost file in the target vhost directory.
+# Written to be run alongside nginxproxy/nginx-proxy from https://hub.docker.com/r/nginxproxy/nginx-proxy.
+# I run it as a cron job to automate new host creation while 
+
+# If LINODE_INTEGRATION is set to True, also attempts to create the domain record.
 
 # Determining script's path and changes to it. Makes paths a bit easier. 
 scriptPath = os.path.realpath(os.path.dirname(__file__))
@@ -19,33 +19,32 @@ os.chdir(scriptPath)
 
 # Creating the log file if it doesn't exist.
 try:
-    if not os.path.exists(logFile):
-        with open(logFile, 'w'): pass
+    if not os.path.exists(LOG_FILE_PATH):
+        with open(LOG_FILE_PATH, 'w'): pass
 except Exception as e:
     raise e
 
 # Throw errors if vital paths do not exist.
-if not os.path.exists(containersPath):
-    raise Exception(f'Directory {containersPath} not present.')
-if not os.path.exists(vhostPath):
-    raise Exception(f'Directory {vhostPath} not present.')
+if not os.path.exists(f'../{CONTAINERS_PATH}'):
+    raise Exception(f'Directory {CONTAINERS_PATH} not present.')
+if not os.path.exists(f'../{VHOSTD_PATH}'):
+    raise Exception(f'Directory {VHOSTD_PATH} not present.')
 
 # Finding all compose files.
-files = [path for path in Path(containersPath).rglob('*') if path.name in ('compose.yml', 'docker-compose.yml')]
-
+files = [path for path in Path(f'../{CONTAINERS_PATH}').rglob('*') if path.name in ('compose.yml', 'docker-compose.yml')]
 if not files:
     raise Exception('No compose.yml or docker-compose.yml files found.')
 
 def timestamp():
     # Returns the current time and date as a formatted string.
-    return dt.now().strftime('%Y/%m/%d %H:%M: ')
+    return dt.now().strftime(TIMESTAMP_FORMAT)
 
 def writeLog(logType: str, exception: Exception, object: str):
     # Takes a log type, and writes to the file depending on the type.
     # Also ensures the log file never exceeds 1000 lines.
     if logType == 'error':
         try:
-            with open(logFile, 'r+') as f:
+            with open(LOG_FILE_PATH, 'r+') as f:
                 lines = f.readlines()
                 if len(lines) > 1000:
                     lines.pop(0)
@@ -59,7 +58,7 @@ def writeLog(logType: str, exception: Exception, object: str):
             raise e
     elif logType == 'info':
         try:
-            with open(logFile, 'r+') as f:
+            with open(LOG_FILE_PATH, 'r+') as f:
                 lines = f.readlines()
                 if len(lines) > 1000:
                     lines.pop(0)
@@ -75,12 +74,12 @@ def writeLog(logType: str, exception: Exception, object: str):
         raise Exception('Invalid log type.')
 
 def main():
-    # Writing default inital host file if it doesn't exist. 
+    # Writing default initial host file if it doesn't exist. 
     try:
-        if not os.path.exists(f'{vhostPath}/{defaultHost}'):
+        if not os.path.exists(f'../{VHOSTD_PATH}/{DEFAULT_HOST}'):
             try:
-                with open(f'{vhostPath}/{defaultHost}', 'w') as f:
-                    f.write(defaultVhost)
+                with open(f'../{VHOSTD_PATH}/{DEFAULT_HOST}', 'w') as f:
+                    f.write(DEFAULT_VHOST_TEXT)
             except:
                 return Exception(f'Unable to write initial vhost file.\n{e}')
     except Exception as e:
@@ -93,13 +92,31 @@ def main():
 
             rgx = re.search('(?<=VIRTUAL_HOST=).+', currentFile)
             if rgx:
-                if (not Path(f'{vhostPath}/{rgx[0]}').is_file()) and (rgx[0] != 'DEFAULT_HOST'):
+                if (not Path(f'../{VHOSTD_PATH}/{rgx[0]}').is_file()) and (rgx[0] != 'DEFAULT_HOST'):
                     try:
-                        shutil.copy(f'{vhostPath}/{defaultHost}', f'{vhostPath}/{rgx[0]}')
+                        shutil.copy(f'../{VHOSTD_PATH}/{DEFAULT_HOST}', f'../{VHOSTD_PATH}/{rgx[0]}')
                     except Exception as e:
                         writeLog('error', e, rgx[0])
                     else:
                         writeLog('info', '', object=rgx[0])
+                    if LINODE_INTEGRATION:
+                        if not createRecord(rgx[0], 'A'):
+                            raise Exception('Vhost created, but Linode record creation failed.')
+                        else:
+                            try:
+                                with open(LOG_FILE_PATH, 'r+') as f:
+                                    lines = f.readlines()
+                                    if len(lines) > 1000:
+                                        lines.pop(0)
+                                        lines.append(f'Successfully created {rgx[0]} domain record.\n')
+                                        f.seek(0)
+                                        f.truncate()
+                                        f.writelines(lines)
+                                    else:
+                                        f.writelines(f'Successfully created {rgx[0]} domain record.')
+                            except Exception as e:
+                                raise e
+                            
     except Exception as e:
         raise e
 
